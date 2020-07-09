be515dbfbe9e275a3a0872c92bab834587fc4844
Benarji <benarji556612@gmail.com>
2020-07-09 08:37:15 +0530

    Updates

---
+++ precache-manifest.ac5f619beecf4aeb6d21c1354cdea851.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10402dc37f1e3797f49e2562e2c6bfe2",
+    "url": "/Movie-Quote-Search/index.html"
+  },
+  {
+    "revision": "629d58cb11e72b55bbaf",
+    "url": "/Movie-Quote-Search/static/css/main.c94e5f3a.chunk.css"
+  },
+  {
+    "revision": "7ffd1e7d4f92358e5856",
+    "url": "/Movie-Quote-Search/static/js/2.e50eee64.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Movie-Quote-Search/static/js/2.e50eee64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629d58cb11e72b55bbaf",
+    "url": "/Movie-Quote-Search/static/js/main.04e04d9a.chunk.js"
+  },
+  {
+    "revision": "7f3b9620410f1a4bb48f",
+    "url": "/Movie-Quote-Search/static/js/runtime-main.a7faa2c3.js"
+  }
+]);
